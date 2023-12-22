72064e1dc0bdc78464556290d6af918cbb33ba9d
shinj <shinj@LAPTOP-4TIVDFN5>
2023-12-22 09:30:46 +0900

    Initial vesion

--- GitHW/src/mygit/YourClass.java
+++
@@ -1,8 +0,0 @@
-package mygit;
-
-public class YourClass {
-	public void sayHello(String toWhom) {
-		System.out.println("Hello "+toWhom);
-		System.out.println("My changed in master branch");
-	}
-}
